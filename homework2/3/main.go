62d96fa724cc83cdaeb469c9c92069be64babfbc
konstantin <kostya.and83@gmail.com>
2021-11-26 10:09:53 +0300

    fixed

--- homework2/3/main.go
+++ homework2/3/main.go
@@ -7,9 +7,9 @@ func main() {
 	var i int
 	fmt.Println("Введите  трехзначное число")
 	fmt.Scanln(&i)
-	var a = i / 100
-	var b = i / 10
-	var c = i / 1
+	var a = (i - (i%100))/1
+	var b = (i - (i%10))/10
+	var c = (i - (i%1))/100
 	fmt.Println("Количество единиц:", a)
 	fmt.Println("Количество десятков:", b)
 	fmt.Println("Количество сотен:", c)
